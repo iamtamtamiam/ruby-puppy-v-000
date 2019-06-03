c04940d22cd8286e1ac48c85b06be04772c87a95
Tam Vu <vutam_a@hotmail.com>
2019-06-03 21:47:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+   @@all = []
+  attr_accessor :name 
+ 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all
+    @@all.each do |@names|
+      puts @names
+    end 
+  end 
+  
+end 
